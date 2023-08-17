4c54b7665c74075dffb418995b7f4d437d9ca37f
Adarsh <Adarsh.Singh@adform.com>
2023-08-17 16:47:22 +0530

    Update

--- src/main/java/org/example/Consumer.java
+++ src/main/java/org/example/Consumer.java
@@ -1,7 +1,6 @@
 package org.example;
 
-import io.confluent.kafka.serializers.protobuf.KafkaProtobufDeserializer;
-import io.confluent.kafka.serializers.protobuf.KafkaProtobufDeserializerConfig;
+import com.google.protobuf.InvalidProtocolBufferException;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -9,13 +8,12 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 
 public class Consumer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidProtocolBufferException {
         Logger logger = LoggerFactory.getLogger(Consumer.class);
         String bootstrapServers = "127.0.0.1:9092";
         String grp_id = "second_app";
@@ -26,19 +24,15 @@ public class Consumer {
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, CustomDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG,grp_id);
-        properties.put(KafkaProtobufDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://127.0.0.1:8081");
-        properties.put(KafkaProtobufDeserializerConfig.SPECIFIC_PROTOBUF_VALUE_TYPE, MyRecord.Record.class.getName());
-//        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
         // Creating consumer
-        KafkaConsumer<String ,Customer> consumer = new KafkaConsumer<>(properties);
+        KafkaConsumer<String , Customer> consumer = new KafkaConsumer<>(properties);
         consumer.subscribe(Arrays.asList(topic));
         while(true){
-            ConsumerRecords<String,Customer> records = consumer.poll(Duration.ofMillis(100));
-            for (ConsumerRecord<String,Customer> record:records){
+            ConsumerRecords<String, Customer> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, Customer> record:records){
                 logger.info("key: "+ record.key() + ", Value: " + record.value());
                 logger.info("Partition: " + record.partition() + ", Offset: " + record.offset());
             }
         }
     }
 }
-
--- src/main/java/org/example/Producer.java
+++ src/main/java/org/example/Producer.java
@@ -1,36 +1,25 @@
 package org.example;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.confluent.kafka.serializers.protobuf.KafkaProtobufSerializer;
-import io.confluent.kafka.serializers.protobuf.KafkaProtobufSerializerConfig;
 import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
 import java.util.Properties;
-import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 
 public class Producer {
-    public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
+    public static void main(String[] args) throws ExecutionException, InterruptedException{
         final Logger logger = LoggerFactory.getLogger(Producer.class);
-        // Converting to Json format
-        Customer customer = new Customer(231,"Adarsh Singh");
-        MyRecord.Record r = MyRecord.Record.newBuilder().setId(1).setLocation("Mumbai").setName("Adarsh").build();
         // Creating properties
         String bootstrapServers = "127.0.0.1:9092";
-        String schema = "127.0.0.1:8081";
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServers);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, CustomSerializer.class.getName());
-        properties.put(KafkaProtobufSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG,schema);
         // Creating producer
         KafkaProducer<String, Customer> first_producer = new KafkaProducer<>(properties);
         String topic = "newTopic";
+        Customer customer = new Customer(231,"Adarsh Singh");
         for(int i=0;i<10;i++){
             String key = "id_" + i;
             ProducerRecord<String, Customer> record = new ProducerRecord<>(topic,key,customer);
@@ -51,4 +40,3 @@ public class Producer {
         first_producer.close();
     }
 }
-
